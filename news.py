7bb81e562ea91c352e2cdd6e1599d6de1559faf9
Haroon <6649831+H-Wardak@users.noreply.github.com>
2018-11-18 16:05:24 +0300

    Add exception hadler to DB connection, Enhance query 1, rewrite query 2

--- news.py
+++ news.py
@@ -1,22 +1,30 @@
-# Database Code for news DB, python3
+#!/usr/bin/env python3
 import psycopg2
 
 
 # connect to, and fetch result from DB
 # It takes two arguments, DB name and the query
 def get_query_rslt(db_name, query):
-    db = psycopg2.connect('dbname={}'.format(db_name))
-    cursor = db.cursor()
-    cursor.execute(query)
-    output = cursor.fetchall()
-    db.close()
-    return output
+	try:
+	    db = psycopg2.connect('dbname={}'.format(db_name))
+	except psycopg2.Error as e:
+	    print "Unable to connect!"
+	    print e.pgerror
+	    print e.diag.message_detail
+	    sys.exit(1)
+	else:
+	    print("Connected to DB")
+        cursor = db.cursor()
+        cursor.execute(query)
+        output = cursor.fetchall()
+        db.close()
+        return output
 
 
 def get_top3_articles():
     query = "SELECT title, count FROM articles, \
     (SELECT slug, count(path) AS count FROM articles, log \
-    WHERE path LIKE '%' || slug || '%' GROUP BY slug) AS subq WHERE \
+    WHERE log.path = '/article/' || articles.slug GROUP BY slug) AS subq WHERE \
     articles.slug = subq.slug ORDER BY count DESC LIMIT 3;"
     output = get_query_rslt('news', query)
     f.write("1- What are the most popular three articles of all time?\n")
@@ -25,12 +33,9 @@ def get_top3_articles():
 
 
 def get_top_authors():
-    query = "SELECT name, views FROM authors, \
-    (SELECT title, author, views FROM articles, \
-    (SELECT slug, count(path) AS views FROM articles, log \
-    WHERE path LIKE '%' || slug || '%' GROUP BY slug) AS subq1 \
-    WHERE articles.slug = subq1.slug) AS subq2 \
-    WHERE authors.id = subq2.author ORDER BY subq2.views DESC;"
+    query = "SELECT name, count(*) AS views FROM authors, log, articles \
+    WHERE authors.id = articles.author AND log.path = '/article/' || slug AND \
+    status ='200 OK' GROUP BY name ORDER BY views DESC;"
     output = get_query_rslt('news', query)
     f.write("\n2- Who are the most popular article authors of all time?\n")
     for author, views in output:
